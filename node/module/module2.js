fd0c4c831b789ce9cb6e5ded6a842e2c9e1213e3
Poype <253186926@qq.com>
2024-05-11 22:42:49 +0800

    [nodejs] CommonJS规范

--- node/module/module2.js
+++ node/module/module2.js
@@ -26,3 +26,9 @@ exports.run = () => {
 
 // module.exports 和 exports 默认指向同一个对象。但可以通过修改使它俩指向不同的对象
 // 导出哪些成员永远由module.exports决定！！
+
+
+// NodeJs基于CommonJS规范实现模块化：
+// 1. 在每个模块内部，module 变量代表了当前模块
+// 2. module变量是一个对象，它的 exports 属性是对外导出的成员。
+// 3. 调用require方法加载某个模块，其实是加载那个模块的 module.exports属性
