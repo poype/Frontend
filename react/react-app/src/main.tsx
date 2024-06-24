4c860b2ec1f2bd205d33e0658c107ea390bf8690
Poype <253186926@qq.com>
2024-06-24 22:17:27 +0800

    How React Works

--- react/react-app/src/main.tsx
+++ react/react-app/src/main.tsx
@@ -7,3 +7,18 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
     <App />
   </React.StrictMode>
 );
+
+/*
+  How React Works:
+
+  React takes the component tree and build a JavaScript data structure called the virtual DOM.
+  When the state or the data of a component changes, React updates the corresponding nodes in the virtual DOM.
+  Then it compares the current version of virtual DOM with the previous version to identity the nodes that should be updated.
+  It will update those nodes in the actual DOM.
+  Technically, updating the actual DOM is not done by React itself, it's done by library called React DOM.
+
+  React is platform agnostic.
+  here we use ReactDOM to render components into web page, 
+  we can also render components tree in a mobile app using different libaray called React Native. 
+  We can use react to build web, mobile app and destop devices.
+ */
