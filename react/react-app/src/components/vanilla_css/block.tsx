327d537cef044c4b64ace994f480bb254e041473
Poype <253186926@qq.com>
2024-07-04 06:50:13 +0800

    css modules

--- react/react-app/src/components/vanilla_css/block.tsx
+++ react/react-app/src/components/vanilla_css/block.tsx
@@ -1,9 +1,16 @@
-import "./block.css"
+import styles from "./block.module.css"
+// styles here is just a regular javascript object that has all the classes we have defined in the file.
+// every classes we defined in the file is going to be a property on that object.
 
 function Block() {
 
   return (
-    <div className="vanilla-css-1"></div>
+    <>
+      <div className={styles.vanillaCss1}></div>
+      {/* mutiple css class name */}
+      <div className={[styles.one, styles.two].join(' ')}></div>
+    </>
+    
   );
 }
 
