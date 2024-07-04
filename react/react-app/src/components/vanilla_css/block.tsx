e98c7159e3f0cd49b4bc1200074ca568ff36b612
Poype <253186926@qq.com>
2024-07-04 09:01:47 +0800

    react-icons

--- react/react-app/src/components/vanilla_css/block.tsx
+++ react/react-app/src/components/vanilla_css/block.tsx
@@ -2,6 +2,8 @@ import styles from "./block.module.css"
 // styles here is just a regular javascript object that has all the classes we have defined in the file.
 // every classes we defined in the file is going to be a property on that object.
 
+import { CiCalendar } from "react-icons/ci";
+
 function Block() {
 
   return (
@@ -11,6 +13,9 @@ function Block() {
       <div className={[styles.one, styles.two].join(' ')}></div>
 
       <p style={{backgroundColor: "blue", color: "red"}}>test inline css</p>
+
+      <CiCalendar color="gold" size={40}/>
+      {/* https://react-icons.github.io/react-icons/ */}
     </>
     
   );
