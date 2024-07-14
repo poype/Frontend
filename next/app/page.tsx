69f264a63ab5ce26e66ebcd1e23b0fb13cddf11a
Poype <253186926@qq.com>
2024-07-14 11:44:36 +0800

    routing and nagivation

--- next/app/page.tsx
+++ next/app/page.tsx
@@ -1,7 +1,14 @@
-import Image from 'next/image'
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <main><h1>Hello World</h1></main>
-  )
+    <main>
+      <h1>Hello World</h1>
+      {/* <a href="/users">Users</a> */}
+      {/* use a tag for navigation will reload all resources, but we only want to download the resource of new page */}
+
+      <Link href="/users">Users</Link>
+      {/* only download the resource of users page, it will not redownload font, css... */}
+    </main>
+  );
 }
