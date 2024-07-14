6a54f1a37e7ccac167d6cd70c595ec8465ae408d
Poype <253186926@qq.com>
2024-07-14 14:49:32 +0800

    Server components cannot ...

--- next/app/components/ProductCard.tsx
+++ next/app/components/ProductCard.tsx
@@ -14,3 +14,10 @@ const ProductCard = () => {
 export default ProductCard;
 
 // All components inside the app folder are server components by default.
+
+// Server components cannot
+// 1. Listen to browser events
+// 2. Access browser APIs
+// 3. Maintain state
+// 4. Use effects
+// These functionalities are only available in client components.
