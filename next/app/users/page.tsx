2c4c93410750f6cc90a792e59a0fd4338d4032af
Poype <253186926@qq.com>
2024-07-14 17:13:59 +0800

    cache parameter

--- next/app/users/page.tsx
+++ next/app/users/page.tsx
@@ -7,7 +7,23 @@ interface User {
 
 const UsersPage = async () => {
   // in the server component, we can use the fetch funtion to send HTTP requests to the backend.
-  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  // const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  
+  // whenever we use the fetch function to get some data, Next.js will automatically store the result in date cache which is based on the file system.
+  
+  // to disable caching
+  // const res = await fetch(
+  //   "https://jsonplaceholder.typicode.com/users", 
+  //   {cache: 'no-store'}
+  // );
+
+  // to keep data fresh for a certain period of time
+  // next.js is going to run a background job and get fresh data from backend every 10 seconds.
+  const res = await fetch(
+    "https://jsonplaceholder.typicode.com/users", 
+    {next: { revalidate: 10 }}
+  );
+  
   const users: User[] = await res.json();
 
   return (
