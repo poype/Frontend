39d8e04c51e124603a93b799415fb23990937195
Poype <253186926@qq.com>
2024-07-14 14:44:15 +0800

    data fetching

--- next/app/users/page.tsx
+++ next/app/users/page.tsx
@@ -1,7 +1,23 @@
 import React from "react";
 
-const UsersPage = () => {
-  return <div> UsersPage </div>;
+interface User {
+  id: number;
+  name: string;
+}
+
+const UsersPage = async () => {
+  // in the server component, we can use the fetch funtion to send HTTP requests to the backend.
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const users: User[] = await res.json();
+
+  return (
+    <>
+      <h1>Users</h1>
+      <ul>
+        {users.map(user => <li key={user.id}>{user.name}</li>)}
+      </ul>
+    </>
+  );
 };
 
 export default UsersPage;
