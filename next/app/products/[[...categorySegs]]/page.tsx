e9f192c7242f251c85501a0ab9fd4a6ce411d9aa
Poype <253186926@qq.com>
2024-07-24 22:31:12 +0800

    query string parameter

--- next/app/products/[[...categorySegs]]/page.tsx
+++ next/app/products/[[...categorySegs]]/page.tsx
@@ -3,10 +3,13 @@ import React from 'react'
 interface Props {
   params: {
     categorySegs: string[]
+  },
+  searchParams: {
+    searchOrder: string;
   }
 }
 
-const ProductPage = ({ params: { categorySegs } }: Props) => {
+const ProductPage = ({ params: { categorySegs }, searchParams: { searchOrder } }: Props) => {
 
   return (
     <div>
@@ -14,8 +17,13 @@ const ProductPage = ({ params: { categorySegs } }: Props) => {
       <ul>
         {categorySegs && categorySegs.map((category) => <li>{category}</li>)}
       </ul>
+
+      searchOrder: {searchOrder}
     </div>
   )
 }
 
 export default ProductPage
+
+
+// url: http://localhost:3000/products/aa/bb/cc/dd?searchOrder=price
