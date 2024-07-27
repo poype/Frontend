11c7ab34559ee7e714daaa83a46c6040196fd792
Poype <253186926@qq.com>
2024-07-27 11:17:21 +0800

    prisma CRUD

--- next/app/api/users/[id]/route.tsx
+++ next/app/api/users/[id]/route.tsx
@@ -1,18 +1,24 @@
 import { NextRequest, NextResponse } from "next/server";
+import prisma from "@/prisma/client";
 
 interface Props {
   params: {
-    id: number;
+    id: string;
+    // even though the declare you give here is number, the actual type is string, so you have to give string type here.
   };
 }
 
-export function GET(request: NextRequest, { params: { id } }: Props) {
-  if (id > 10) {
+export async function GET(request: NextRequest, { params: { id } }: Props) {
+  const user = await prisma.user.findUnique({
+    where: { id: parseInt(id) },
+  });
+
+  if (!user) {
     // the second parameter is http status code
     return NextResponse.json({ error: "user not found" }, { status: 404 });
   }
 
-  return NextResponse.json({ id: 2, name: "Mosh" });
+  return NextResponse.json(user);
 }
 
 interface UpdateUserRequest {
@@ -25,18 +31,39 @@ export async function PUT(request: NextRequest, { params: { id } }: Props) {
     return NextResponse.json({ error: "name is required" }, { status: 400 });
   }
 
-  if (id > 10) {
+  const user = await prisma.user.findUnique({
+    where: {
+      id: parseInt(id),
+    },
+  });
+
+  if (!user) {
     return NextResponse.json({ error: "User Not Found" }, { status: 404 });
   }
 
-  return NextResponse.json({ id: 1, name: updateUserRequest.name });
+  const updatedUser = await prisma.user.update({
+    where: { id: user.id },
+    data: {
+      // only update name
+      name: updateUserRequest.name,
+    },
+  });
+
+  return NextResponse.json(updatedUser);
 }
 
+export async function DELETE(request: NextRequest, { params: { id } }: Props) {
+  const user = await prisma.user.findUnique({
+    where: { id: parseInt(id) },
+  });
 
-export function DELETE(request: NextRequest, { params: { id } }: Props) {
-  if (id > 10) {
+  if (!user) {
     return NextResponse.json({ error: "User Not Found" }, { status: 404 });
   }
+
+  await prisma.user.delete({
+    where: { id: user.id },
+  });
 
   return NextResponse.json({});
 }
--- next/app/api/users/route.tsx
+++ next/app/api/users/route.tsx
@@ -1,28 +1,48 @@
 import { NextRequest, NextResponse } from "next/server";
+import prisma from "@/prisma/client";
 
 // in a given folder or URL segment, we can eigher have a route.tsx file or page.tsx file, but not both.
 
 // GET handler
-export function GET(request: NextRequest) {
-  return NextResponse.json([
-    { id: 1, name: "John" },
-    { id: 2, name: "Mosh" },
-  ]);
+export async function GET(request: NextRequest) {
+  const users = await prisma.user.findMany();
+  return NextResponse.json(users);
 }
 
 interface AddUserRequest {
   name: string;
+  email: string;
 }
 
 export async function POST(request: NextRequest) {
   const addUserRequest: AddUserRequest = await request.json();
 
-  if (!addUserRequest.name) {
-    return NextResponse.json({ error: "name is required" }, { status: 400 });
+  if (!addUserRequest.name || !addUserRequest.email) {
+    return NextResponse.json(
+      { error: "name and email is required" },
+      { status: 400 }
+    );
   }
+
+  const cnt = await prisma.user.count({
+    where: { email: addUserRequest.email },
+  });
+
+  if (cnt > 0) {
+    return NextResponse.json(
+      { error: "User already exists" },
+      { status: 400 }
+    );
+  }
+
+  // save user
+  const user = await prisma.user.create({
+    data: {
+      name: addUserRequest.name,
+      email: addUserRequest.email,
+    },
+  });
+
   // 201 indicate that an object was created
-  return NextResponse.json(
-    { name: addUserRequest.name, id: 101 },
-    { status: 201 }
-  );
+  return NextResponse.json(user, { status: 201 });
 }
