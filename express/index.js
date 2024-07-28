43abf625a63b85c14d76fadae35c059544e8e166
Poype <253186926@qq.com>
2024-07-28 21:47:39 +0800

    Reading Environment Variables

--- express/index.js
+++ express/index.js
@@ -6,6 +6,7 @@ app.get("/", (req, res) => {
   res.send("Hello Express");
 });
 
-app.listen(3000, () => {
-  console.log("Listening on port 3000...");
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Listening on port ${port}...`);
 });
