49500ff050d7a827bd03ce9f9020b582a2782d9e
Poype <253186926@qq.com>
2024-07-31 22:30:51 +0800

    Type Aliases

--- typescript/hello-world/src/object.ts
+++ typescript/hello-world/src/object.ts
@@ -9,3 +9,19 @@ let employee: {
 };
 
 employee.retire(new Date());
+
+type Employee = {
+  readonly id: number;
+  name: string;
+  retire: (date: Date) => void;
+};
+
+let employee2: Employee = {
+  id: 2,
+  name: "Marco",
+  retire: (date: Date) => console.log(date),
+};
+
+console.log(employee2.id);
+console.log(employee2.name);
+employee2.retire(new Date())
