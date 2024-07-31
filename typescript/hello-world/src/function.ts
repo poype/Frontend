33d2fceda5bf5471b035b9737825a1786b7a7475
Poype <253186926@qq.com>
2024-07-31 22:07:27 +0800

    typescript function

---
+++ typescript/hello-world/src/function.ts
@@ -0,0 +1,12 @@
+
+
+function calculateTax(income: number, taxYear: number = 2022): number {
+  if (taxYear < 2022) {
+    return income * 1.2;
+  }
+  return income * 1.3;
+}
+
+calculateTax(10_000, 2019);
+calculateTax(10_000, 2024);
+calculateTax(10_000);
