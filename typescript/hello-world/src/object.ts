39bf78c26bb6822681eeff435861160676586f34
Poype <253186926@qq.com>
2024-08-01 08:40:02 +0800

    Literal Types

--- typescript/hello-world/src/object.ts
+++ typescript/hello-world/src/object.ts
@@ -24,4 +24,14 @@ let employee2: Employee = {
 
 console.log(employee2.id);
 console.log(employee2.name);
-employee2.retire(new Date())
+employee2.retire(new Date());
+
+
+type Quantity = 50 | 100;
+
+// quantity can only be assigned 50 or 100
+let quantity: Quantity = 50;
+
+type Metric = "cm" | "inch";
+
+let metric: Metric = "cm";
