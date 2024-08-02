061658392711e7a572d8b7d537710f14b1625faf
Poype <253186926@qq.com>
2024-08-02 21:03:42 +0800

    static

--- typescript/hello-world/src/class.ts
+++ typescript/hello-world/src/class.ts
@@ -60,3 +60,29 @@ let seats = new SeatAssignment();
 seats.A1 = "Mosh";
 seats["A2"] = "John";
 // seats.A3 = 1;   error
+
+// static properties belong to class, not object
+class Ride {
+  // use private here is to prevent the value of variable from being modified elsewhere.
+  private static _activeRides: number = 0;
+
+  start() {
+    Ride._activeRides++;
+  }
+
+  stop() {
+    Ride._activeRides--;
+  }
+
+  static get activeRides() {
+    return Ride._activeRides;
+  }
+}
+
+let ride1 = new Ride();
+ride1.start();
+
+let ride2 = new Ride();
+ride2.start();
+
+console.log("active rides is: " + Ride.activeRides);
