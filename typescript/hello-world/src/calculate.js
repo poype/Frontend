753db169b9cdfba3b3cd967810f86c455d6b0fec
Poype <253186926@qq.com>
2024-08-04 07:36:43 +0800

    use js-doc to provide type information

---
+++ typescript/hello-world/src/calculate.js
@@ -0,0 +1,9 @@
+/**
+ * adding calculating
+ * @param {number} num1 - first number
+ * @param {number} num2 - second number
+ * @returns {number} calculating result
+ */
+export function add(num1, num2) {
+  return num1 + num2;
+}
--- typescript/hello-world/src/taxTs.ts
+++ typescript/hello-world/src/taxTs.ts
@@ -1,5 +1,8 @@
 import { calculateTax } from "./taxJs";
+import { add } from "./calculate";
 
 console.log(calculateTax(100));
 
 console.log(calculateTax()); // NaN
+
+console.log(add(1, 2));
