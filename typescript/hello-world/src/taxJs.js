991fff6df71e4e7a0a712b323374c18d0cd31362
Poype <253186926@qq.com>
2024-08-04 07:28:43 +0800

    enable type checking for JS

--- typescript/hello-world/src/taxJs.js
+++ typescript/hello-world/src/taxJs.js
@@ -1,3 +1,7 @@
+// @ts-nocheck
 export function calculateTax(income) {
   return income * 0.3;
 }
+
+
+// @ts-nocheck make ts complier to ignore type checking for that function
--- typescript/hello-world/src/taxTs.ts
+++ typescript/hello-world/src/taxTs.ts
@@ -1,3 +1,5 @@
 import { calculateTax } from "./taxJs";
 
 console.log(calculateTax(100));
+
+console.log(calculateTax()); // NaN
