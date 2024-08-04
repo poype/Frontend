5d9e40e8917d65c98d347d50abbcae33ebc24178
Poype <253186926@qq.com>
2024-08-04 08:09:48 +0800

    DefinitelyTyped project provide declaration files for exist popular JavaScript libs

--- typescript/hello-world/src/index.ts
+++ typescript/hello-world/src/index.ts
@@ -1,1 +1,9 @@
-let numbers: number[] = [1, 2, 3];
+import * as _ from 'lodash'
+
+// loadash is a pure javascript library
+// it doean't have js-doc comments or declaration files 
+
+// we can use DefinitelyTyped to reaolve this issue.
+// DefinitelyTyped is a very popular project in github, 
+// we can find declaration files for all the popular javascript libraries in it.
+
